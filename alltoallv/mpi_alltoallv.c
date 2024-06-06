7015c537ab72bbbc5e249c5d6b81361d21fc6ecb
Geoffroy Vallee <geoffroy.vallee@gmail.com>
2024-06-06 18:03:33 +0200

    Fix copyright statements

--- alltoallv/mpi_alltoallv.c
+++ alltoallv/mpi_alltoallv.c
@@ -1,6 +1,6 @@
 /*************************************************************************
- * Copyright (c) 2019-2010, Mellanox Technologies, Inc. All rights reserved.
- * Copyright (c) 2020-2023, NVIDIA CORPORATION. All rights reserved.
+ * Copyright (c) 2019-2020, Mellanox Technologies, Inc. All rights reserved.
+ * Copyright (c) 2020-2024, NVIDIA CORPORATION. All rights reserved.
  *
  * See LICENSE.txt for license information
  ************************************************************************/
